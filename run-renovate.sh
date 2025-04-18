e16baf41dfb0002834119719f9f9b473deb3bd7c
steffsas <steffen.sassalla@outlook.de>
2025-04-18 09:32:17 +0200

    Fix vars in bash script

--- run-renovate.sh
+++ run-renovate.sh
@@ -8,7 +8,7 @@ fi
 
 docker run $DOCKER_FLAGS \
     -v "$(pwd)":/usr/src/app \
-    -e LOG_LEVEL="$LOG_LEVEL" \
-    -e RENOVATE_PLATFORM="$RENOVATE_PLATFORM" \
-    -e GITHUB_COM_TOKEN="$GITHUB_COM_TOKEN" \
+    -e LOG_LEVEL=debug \
+    -e RENOVATE_PLATFORM=local \
+    -e GITHUB_COM_TOKEN=dummy \
     renovate/renovate
